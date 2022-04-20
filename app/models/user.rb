4522a62f79652cabf08f1758d45166612e7fdf11
kao126 <76607249+kao126@users.noreply.github.com>
2022-04-20 09:12:34 +0000

    [Update] 4-25

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_one_attached :profile_image
+  has_many :books, dependent: :destroy
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get '/homes/about'
+  devise_for :users
+
+  resources :books, only: [:index, :create, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
