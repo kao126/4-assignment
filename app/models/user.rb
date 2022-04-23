1ab26b43ff07fe1938070c140b31f3a46509a11c
kao126 <76607249+kao126@users.noreply.github.com>
2022-04-23 03:32:37 +0000

    [Update/Finish] 4-25(After Rspecテスト)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-  def get_profile_image
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/default-image.jpeg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
     end
-    profile_image
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
   # Include default devise modules. Others available are:
